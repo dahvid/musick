74703712319050516fc34bc33a3271eeaba92793
David Minor <davidminor@Davids-Mac-mini.local>
2019-04-22 12:06:09 +0300

    on the way

--- music.py
+++ music.py
@@ -1,4 +1,5 @@
 from fractions import Fraction
+from graphutil import Graph
 import attr
 import music21
 
@@ -10,49 +11,123 @@ how to describe these?  A score assumes an x,y relationship, this is adequate fo
 
     There are many more deep relations in music, some invariant (like those mentioned above) and some can be chosen by the composer as canons for guiding his composition
     
+    
+    This has been moved to a modifier function
+        :param direction: slope of line in radians as Fraction(x,y) = xPI/y default is 1/2 (forward)
+                      3/2  is retrograde, 0 is up chord (from center note), 1/2 is downchord,
+                      all other angles will be arpeggios calculated according to the note duration
+                      fraction and the center duration
 '''
 
-
+"""
 #+- an undefined center tone
 @attr.s
 class Interval(object):
     interval = attr.ib(default=0)
+"""
 
 #A fraction of an undefined time span
-@attr.s
-class Duration(object):
-    duration = attr.ib(default=Fraction(1))
 
-#+- an undefined center volume
-@attr.s
-class Accent(object):
-    accent = attr.ib(default=1)
+class Duration():
+    def __init__(self, multiple=None, unit='1/4'):
+        self.multiple = multiple
+        self.unit     = Fraction(unit)
+        self.value    = self.multiple * self.unit
+        self.original = unit
 
+"""
 @attr.s
 class Sequence(object):
+     
+    :param object is a list of durations or intervals or motifs
+    A modifier is a function f(t) which can change the values of a sequence
+    t can be a single global change, or a per object change, in which case it
+    will be applied for every duration tick during rendering
+    a modifier can give an accent pattern to a sequence
+     
     sequence = attr.ib(default=[])
+    modifier = attr.ib(default=None)
+"""
 
-@attr.s
-class Motif(object):
-    intervals = attr.ib(default=[])
-    durations = attr.ib(default=[Fraction(1,4) for x in intervals])
-    accents   = attr.ib(default=[1 for x in durations])
+class Motif():
 
-def play(motif, center_pitch=60, center_duration=Fraction(1,2), center_accent=60, direction=Fraction(0)):
+    counter = 0
+
+    """
+    :param intervals:sequence
+    :param durations:sequence
+    :param modifier:function
+    """
+    def __init__(self, name=None, intervals=[], durations=None, modifier=None):
+        if not name:
+            name = 'unique_name_' + str(Motif.counter)
+            Motif.counter += 1
+        self.name = name
+        if not durations:
+            durations = attr.ib(default=[Fraction(1,4) for x in intervals])
+        self.intervals  = []
+        self.durations  = durations
+        self.modifier   = modifier
+        self.offset     = 0
+
+def retrograde(m):
+    return Motif(m.name + '_retro', m.intervales.reverse(), m.durations.reverse())
+
+class Orchestration(Graph):
+    """
+    :param Graph(V = [motif], E = (motif:duration -> motif:duration))
+    :param modifier:function
+
+    """
+    def __init__(self, motifs):
+        self.graph = Graph()
+        for m in motif:
+            self.graph.add_node(m.name, m)
+
+
+def render(orchestration, center=60, tempo=120, modifier=None):
     """
 
-    :param motif:
-    :param center_duration:
-    :param center_accent:
-    :param direction: slope of line in radians as Fraction(x,y) = xPI/y default is 1/2 (forward)
-                      3/2  is retrograde, 0 is up chord (from center note), 1/2 is downchord,
-                      all other angles will be arpeggios calculated according to the note duration
-                      fraction and the center duration
-    :return:
+    :param orchestration:
+    :param center:
+    :param tempo:
+    :param modifier:
+
+    :return: piece
     """
-    pass
+
+
+
 		
 #the idea is that everything is abstract relationships until instantiate
 #and then all functions are evaluated and sequences are given actual 
 #pitchs and durations are give actual time stamps based
-def SinOsc(divisions, )
+
+"""
+We have a collection of ordered intervales which can be re-ordered with modulating functions
+We have a collection of ordered durations which can be re-ordered with modulating functions
+interval group + duration group = motif
+
+A piece is an ordered set of motifs with modulating functions.  So a melody that modulates up a fifth each
+time it is played would be a piece with a single motif and a modulating function that adds a fifth 
+recursively to each of its objects
+
+When a piece is played it needs to be instantiatedd with a center pitch and a center duration and direction, all other
+pitches ad duratioons will be dependent on this
+direction can be used to define chords or arpegios (how are durations releated to direction?)
+
+???is there a way to abstract this futherr???
+intervals could be defined as fractions?  or are they the basis?
+
+It seems like motif and piece are arbitrary, maybe it should be just one recursive structure
+How is simultaneoty dealt with? A conterpoint line is just two motifs played at 1/2 radians, but the 
+relation between them is like a chord (0?)
+
+Maybe vectors makes more sense, phi is the angle (radians) and the length indicates relative duration
+all durations along the vector will be made to fit.  What about pitches?  Intervals can also be stretchd along vectors
+
+
+
+
+
+"""
--- test.py
+++ test.py
@@ -1,8 +1,52 @@
-from music import Sequence,Duration,Motif,Fraction
+from music import *
 
-seq = Sequence([3,-1,2])
-dur = Duration([1, Fraction(1,2), Fraction(1,4), Fraction(1,4)])
 
-mot = Motif(intervals = Sequence)
+m1 = Motif('m1', [1,2,3,5,7])
+m2 = retrograde(m1)
+
+"""
+append 
+start aligned
+end aligned
+
+duration aligned
+
+m1 => m2  append or m1(end) | m2
+m1 | m2   simultaneos or m1(0) => m2
+
+
+m1:23 => m2  23 duration units into m1 start m2
+
+m1:(23,7/8) => m2 #23 7/8 ths into m1 start m2
+m1 => (3,1/4):m2   #at end of m1 start m2 3/4 into
+
+
+overloaded version
+
+m3 = m1(end) >> m2(begin)  #append
+m3 = m1(begin) | m2(begin) #simul
+
+m3 = m1 + end >> m2 #append
+m3 = m1 | m2  #start simul
+
+x >> y #y's start is relative to x
+x | y  #y and x have the same beginning reference point
+
+
+m3 = m1 + d(3,'7/9') >> m2 #start m2  3 7/9ths into m1
+m3 = m1 + d(3,'7/9') | m2 #start m1  3 7/9ths into and m2 immediately
+
+also
+m2 = m1 + d(3,'7/9')
+m2 = m1 - d(3,'7/9') 
+
+
+
+"""
+
+#maybe get rid of orchestrations and just have motifs?
+orc = Orchestration([m1, m2], a)
+
+
 
 
